3c708a033fb78f9e727ecef05aba85c4f710c43d
bengris32 <bengris32@protonmail.ch>
2023-11-18 01:17:23 +0000

    mt6768-common: Switch to AIDL ConsumerIR service

--- mt6768.mk
+++ mt6768.mk
@@ -195,8 +195,7 @@ PRODUCT_COPY_FILES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.mediatek
 
 # Keymaster
 PRODUCT_PACKAGES += \
